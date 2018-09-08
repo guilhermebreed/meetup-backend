b65e9da04ea1c755bcf726989cf50a9756e9a5b8
Guilherme Silva Alves <guilherme.bralves@hotmail.com>
2018-09-08 15:48:41 -0300

    Buscando Meetups por grupo

--- modules/groups/model.js
+++ modules/groups/model.js
@@ -21,18 +21,21 @@ const GroupSchema = new Schema({
   }],
 }, { timestamps: true });
 
+/**
+ * Create a meetup and add it to the meetups array in the group
+ */
 GroupSchema.statics.addMeetup = async function (id, args) {
   const Meetup = mongoose.model('Meetup');
+  // we add the group id to the meetup group element
+  // Finally this is the author of the meetup
+  const meetup = await new Meetup({ ...args, group: id });
 
-  const group = await this.findById(id);
+  const group = await this.findByIdAndUpdate(id, { $push: { meetups: meetup.id } });
 
-  const meetup = await new Meetup({ ...args, group });
-
-  group.meetups.push(meetup);
-
-  const result = await Promise.all([meetup.save(), group.save()]);
-
-  return result;
+  return {
+    meetup: await meetup.save(),
+    group,
+  };
 };
 
 export default mongoose.model('Group', GroupSchema);
